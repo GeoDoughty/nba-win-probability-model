0901ccd6db104d53895a4ff97f1c58b5b2de2d65
George Doughty <144172664+GeoDoughty@users.noreply.github.com>
2025-01-26 18:31:43 +0000

    feat: 🚧 add structure of loop

--- src/data/load_season_pbp_data.py
+++ src/data/load_season_pbp_data.py
@@ -1,17 +1,35 @@
+"""Loads Raw PBP data for a single season.
 """
-Loads Raw PBP data for a single season
-"""
-from nba_api.stats.endpoints import PlayByPlay, LeagueGameLog
+from pathlib import Path
+
+import pandas as pd
+from nba_api.stats.endpoints import LeagueGameLog, PlayByPlay
 
 
 def get_games(season: str | None = None):
     log = LeagueGameLog(
-        season=season
+        season=season,
     )
     games_df = log.get_data_frames()[0]
-    away_mask = games_df['MATCHUP'].str.contains('@')
+    away_mask = games_df["MATCHUP"].str.contains("@")
     return games_df[~away_mask]
 
+def get_single_game_pbp(game_id: str) -> pd.DataFrame:
+    return PlayByPlay(game_id).get_data_frames()[0]
 
-if __name__ == '__main__':
-    games_df = get_games('2022')
+def load_season_pbp_data(games_df: pd.DataFrame, output_path: Path) -> None:
+    if not output_path.parent.exists():
+        output_path.parent.mkdir()
+
+    pbp_list = []
+    for game_id in games_df["GAME_ID"]:
+        single_game_pbp_df = get_single_game_pbp(game_id)
+        pbp_list.append(single_game_pbp_df)
+
+    full_pbp_df = pd.concat(pbp_list)
+    full_pbp_df.to_parquet(output_path)
+
+
+if __name__ == "__main__":
+    games_df = get_games("2022")
+    load_season_pbp_data(games_df, output_path=Path("data/raw/pbp/2022_reg_season.parquet"))
