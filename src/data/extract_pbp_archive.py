d5989c4db9b1dfd48645be452f146f4ba99b65a6
George Doughty <144172664+GeoDoughty@users.noreply.github.com>
2025-03-23 14:48:13 +0000

    feat: ✨ extract out correct data

--- src/data/extract_pbp_archive.py
+++ src/data/extract_pbp_archive.py
@@ -5,14 +5,14 @@ from pathlib import Path
 
 def extract_pbp_from_tar(
     tar_path: Path,
-    output_folder: Path
+    output_path: Path
 ) -> pd.DataFrame:
     with tarfile.open(tar_path, mode='r:xz') as tar:
         files = tar.getmembers()
         file_bytes = tar.extractfile(files[0]).read()
 
     pbp_df = pd.read_csv(BytesIO(file_bytes))
-    pbp_df.to_parquet(output_folder / f"{tar_path.stem}.parquet")
+    pbp_df.to_parquet(output_path)
 
 
 def extract_all_pbp_files(
@@ -22,7 +22,10 @@ def extract_all_pbp_files(
     if not output_folder.exists():
         output_folder.mkdir(parents=True)
     for tar_path in tar_folder.glob("*.tar.xz"):
-        extract_pbp_from_tar(tar_path, output_folder)
+        output_path = output_folder / f"{tar_path.stem}.parquet"
+        if output_path.exists():
+            continue
+        extract_pbp_from_tar(tar_path, output_path)
 
 if __name__ == '__main__':
     extract_all_pbp_files(
