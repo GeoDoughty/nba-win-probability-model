90a1806b5e3660fcd3059e92228c62cbb1c6a212
George Doughty <144172664+GeoDoughty@users.noreply.github.com>
2025-05-24 16:18:26 +0100

    feat: ✨ add logit model training

--- TODO.md
+++ TODO.md
@@ -1,10 +1,6 @@
 ## Short Term
 
-- Resample to 5 second intervals of GAME TIME
-- Create a subset of feature columns (drop evt, loc, ...)
-- Pass to logit model
-
-- [ ] Train Model
+-[x] Figure out why model is predicting 0.58 for every game (is it) - This was due to too many features
 - [ ] Set up basic evals: % accuracy and win prob representation (did 70% of the 70% games win)
 
 Check this out for help: <https://www.digitalocean.com/community/tutorials/logistic-regression-with-scikit-learn>
--- src/model/train_logit.py
+++ src/model/train_logit.py
@@ -7,10 +7,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import numpy as np
 
-train_df = pl.read_parquet("data/processed/resampled_train_22.parquet")
-# test_df = pd.read_parquet("data/processed/test_23.parquet")
 
-index_columns = [
+def prepare_data(df: pl.DataFrame) -> pl.DataFrame:
+    """Prepare the data by converting columns to appropriate types."""
+    return (
+        df.with_columns(
+            pl.col("gametime_elapsed").sub(pl.datetime(1970, 1, 1)).dt.total_seconds()
+        )
+        .select(pl.col(pl.NUMERIC_DTYPES))
+        .drop(INDEX_COLUMNS)
+    ).drop_nulls()
+
+
+train_df = pl.read_parquet("data/processed/resampled_train_22.parquet")
+test_df = pl.read_parquet("data/processed/resampled_test_23.parquet")
+
+INDEX_COLUMNS = [
     "evt",
     "opt1",
     "opt2",
@@ -29,21 +41,46 @@ index_columns = [
     # "PERIOD",
 ]
 
+clean_train_df = prepare_data(train_df)
+clean_test_df = prepare_data(test_df)
 
-clean_train_df = (
-    train_df.with_columns(
-        pl.col("gametime_elapsed").sub(pl.datetime(1970, 1, 1)).dt.total_seconds()
-    )
-    .select(pl.col(pl.NUMERIC_DTYPES))
-    .drop(index_columns)
-).drop_nulls()
+test_cols = [
+    "home_win",
+    "gametime_elapsed",
+    "score_diff",
+    "HOME_ENTRY_W_PCT",
+    "AWAY_ENTRY_W_PCT",
+]
 
-X = clean_train_df.drop("home_win")
-y = clean_train_df["home_win"].to_numpy()
+reduced_train_df = clean_train_df.select(test_cols)
+X = reduced_train_df.drop("home_win")
+y = reduced_train_df["home_win"].to_numpy()
 
 model = LogisticRegression().fit(X=X, y=y)
+
 model.classes_
 X_pred = model.predict_proba(X)[:, 1]
 np.unique(X_pred, return_counts=True)
 
+clean_train_df = clean_train_df.with_columns(home_win_prob=X_pred)
+avg_accuracy = (
+    (clean_train_df["home_win_prob"] > 0.5) == (clean_train_df["home_win"] == 1)
+).mean()
+
+print(f"Average accuracy on training set: {avg_accuracy:.2%}")
+
+# Run on test data
+reduced_test_df = clean_test_df.select(test_cols)
+X_test = reduced_test_df.drop("home_win")
+y = reduced_test_df["home_win"].to_numpy()
+clean_test_df = clean_test_df.with_columns(
+    home_win_prob=model.predict_proba(X_test)[:, 1]
+)
+
+test_avg_accuracy = (
+    (clean_test_df["home_win_prob"] > 0.5) == (clean_test_df["home_win"] == 1)
+).mean()
+
+print(f"Average accuracy on test set: {test_avg_accuracy:.2%}")
+print("you did it chief")
 # Check todo.md for more info
